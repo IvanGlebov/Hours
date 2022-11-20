66e8fa0a16ba2e9781c80808810663ace827b76a
Иван Глебов <glebov.vanya8@gmail.com>
2022-11-21 00:48:47 +0300

    Goals page logic update for redux store usage and styles update

--- src/pages/Goals/Goals.tsx
+++ src/pages/Goals/Goals.tsx
@@ -1,34 +1,51 @@
-import React, { useState } from "react";
-import styles              from "./Goals.module.css";
-import HeaderControls      from "../../components/HeaderControls/HeaderControls";
-import { BarChart, Goal }  from "../../components";
+import React, { useState }  from "react";
+import styles               from "./Goals.module.css";
+import HeaderControls       from "../../components/HeaderControls/HeaderControls";
+import { BarChart, Goal }                 from "../../components";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { getGoals }                       from "../../features/goals/goalsSelectors";
+import { Goal as GoalType } from "../../features/goals/types";
+import { removeOneGoal }    from "../../features/goals/goalsSlice";
 
 const Goals = () => {
 
-  const [goals, setGoals] = useState([
-    { color: "white", hours: 4, tooltip: "Goal 1" },
-    { color: "gray", hours: 4, tooltip: "Goal 2" },
-    { color: "blue", hours: 10, tooltip: "Goal 3" },
-  ])
+  const dispatch = useAppDispatch();
+
+  const goalsEntities = useAppSelector(getGoals);
+
+  const colors: Array<string> = ["#00916E", "#DDFDFE", "#F5CB5C", "#78CAD2", "#A1D2CE", "#AEC3B0", "#EFF6E0"];
+
+  const selectColor = (elIndex: number) => {
+    const index = elIndex % colors.length;
+    return colors[index - 1 < 0 ? 0 : index - 1];
+  }
 
   const removeGoal = (goalIndex: number) => {
-    const newGoals = goals.filter((el, index) => index !== goalIndex);
-    setGoals(newGoals);
+    dispatch(removeOneGoal(goalIndex));
   }
 
   return (
     <div className={styles.container}>
-      <HeaderControls/>
+
       <div className={styles.splitContent}>
-        <div className={styles.goalsTable}>
-          {goals.map((el) =>
-            <Goal name={el.tooltip} removeAction={removeGoal}/>
-          )}
+        <div>
+          <HeaderControls/>
+          <div className={styles.goalsTable}>
+            {Object.values(goalsEntities).map((el) =>
+              <Goal goal={el as GoalType} removeAction={removeGoal}/>
+            )}
+          </div>
         </div>
+
         <div className={styles.sideSection}>
           <h1>Overall</h1>
           <div className={styles.overallBar}>
-            <BarChart itemsArray={goals} maxSum={20}/>
+            <BarChart itemsArray={
+              Object.values(goalsEntities).map(
+                (el) => {
+                return {tooltip: el?.name as string, hours: el?.duration as number, color: selectColor(el?.id as number)}
+              }
+            )} maxSum={300}/>
           </div>
         </div>
       </div>
