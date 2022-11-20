01cb2487669101be411e5bd3e66a14ab364ce3e2
Иван Глебов <glebov.vanya8@gmail.com>
2022-11-21 00:47:00 +0300

    Added 4 initial goals

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect }               from 'react';
 import './App.css';
-import { useSnackbar }      from "notistack";
-import { Navbar }           from "./components";
-import { Route, Routes }    from "react-router";
-import { Goals }           from "./pages";
+import { useSnackbar }                    from "notistack";
+import { Navbar }                         from "./components";
+import { Route, Routes }                  from "react-router";
+import { Goals }                          from "./pages";
+import { addOneGoal }                     from "./features/goals/goalsSlice";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
+import { getNewId }                       from "./features/goals/goalsSelectors";
 
 
 function App() {
 
   const { enqueueSnackbar } = useSnackbar();
+  const newId = useAppSelector(getNewId);
+  const dispatch = useAppDispatch();
 
+  // Adding initial store goals as a filler;
   useEffect(() => {
-      enqueueSnackbar("text", {variant:"success"})
-    }, [])
+    dispatch(addOneGoal({ id: 1, name: "t1", description: "descr", duration: 4 }));
+    dispatch(addOneGoal({ id: 2, name: "t2", description: "descr", duration: 4 }));
+    dispatch(addOneGoal({ id: 3, name: "t3", description: "descr", duration: 4 }));
+    dispatch(addOneGoal({ id: 4, name: "t4", description: "descr", duration: 4 }));
+  }, [])
 
   return (
     <div className="App">
