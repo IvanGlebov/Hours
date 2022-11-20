007c91ce2b3a53053f633ccb3ff20d1632286a6e
Иван Глебов <glebov.vanya8@gmail.com>
2022-11-21 00:47:43 +0300

    Store cleanup

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import goalsReducer from '../features/goals/goalsSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     goals: goalsReducer,
   },
 });
