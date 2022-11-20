dbbd31dce834b98ddc6f25011a7e5c013db36876
Иван Глебов <glebov.vanya8@gmail.com>
2022-11-20 16:34:27 +0300

    Added goals slice into store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import goalsReducer from '../features/goals/goalsSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    goals: goalsReducer,
   },
 });
 
