ac90af977df415249254c10cd446335db068e0bf
Иван Глебов <glebov.vanya8@gmail.com>
2022-11-21 00:49:58 +0300

    Styles update and modal window integration into HeaderControls component

--- src/components/HeaderControls/HeaderControls.tsx
+++ src/components/HeaderControls/HeaderControls.tsx
@@ -1,17 +1,24 @@
-import React          from "react";
+import React, { useState }    from "react";
 import styles                 from "./HeaderControls.module.css"
 import { Button, IconButton } from "@mui/material";
 import AddCircleIcon          from '@mui/icons-material/AddCircle';
+import { AddGoalModal }       from "../index";
 
 const HeaderControls = () => {
+
+  const [open, setOpen] = useState(false);
+  const toggle = () => {
+    setOpen(!open);
+    console.log('TOGGLE');
+  }
+
   return (
     <div className={styles.container}>
-      <IconButton color="primary" onClick={() => {
-        console.log('Click')
-      }}>
+      <IconButton color="primary" onClick={toggle}>
         <AddCircleIcon fontSize="large" />
       </IconButton>
       <Button variant="outlined" size="large">Filters</Button>
+      <AddGoalModal open={open} toggle={toggle}/>
     </div>
   )
 }
